1242128486add850f1d948f94e3fc61c98f1d867
Muhammad Ali <ali@muhammada.li>
2013-05-29 21:38:23 -0400

    Fixing warnings, cleaning imports

--- src/li/muhammada/android/vending/billing/IabException.java
+++ src/li/muhammada/android/vending/billing/IabException.java
@@ -22,22 +22,23 @@ package li.muhammada.android.vending.billing;
  * call {@link #getResult()}.
  */
 public class IabException extends Exception {
-    IabResult mResult;
+	private static final long serialVersionUID = -5941347783171219477L;
+	IabResult mResult;
 
-    public IabException(IabResult r) {
-        this(r, null);
-    }
-    public IabException(int response, String message) {
-        this(new IabResult(response, message));
-    }
-    public IabException(IabResult r, Exception cause) {
-        super(r.getMessage(), cause);
-        mResult = r;
-    }
-    public IabException(int response, String message, Exception cause) {
-        this(new IabResult(response, message), cause);
-    }
+	public IabException(IabResult r) {
+		this(r, null);
+	}
+	public IabException(int response, String message) {
+		this(new IabResult(response, message));
+	}
+	public IabException(IabResult r, Exception cause) {
+		super(r.getMessage(), cause);
+		mResult = r;
+	}
+	public IabException(int response, String message, Exception cause) {
+		this(new IabResult(response, message), cause);
+	}
 
-    /** Returns the IAB result (error) that this exception signals. */
-    public IabResult getResult() { return mResult; }
+	/** Returns the IAB result (error) that this exception signals. */
+	public IabResult getResult() { return mResult; }
 }
